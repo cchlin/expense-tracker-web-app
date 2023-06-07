c4c394fff235fdc1b7a929c538eb282ba836b4ea
Cheng Lin <chlin@pdx.edu>
2023-06-06 23:40:13 -0700

    INSERT new budget group

--- src/models/budget_group_model.rs
+++ src/models/budget_group_model.rs
@@ -7,3 +7,17 @@ pub struct BudgetGroup {
     budget_amount: f64,
     remaining_budget: f64,
 }
+
+impl BudgetGroup {
+    // create a new budget group
+    pub fn create(conn: &Connection, name: String, budget_amount: f64) -> Result<usize> {
+        conn.execute(
+            "INSERT INTO budget_groups (name, budget_amount, remaining_budget) VALUES(?1, ?2, ?3)",
+            params![name, budget_amount, budget_amount],
+        )?;
+
+        // Get the id of the last inserted row
+        let id = conn.last_insert_rowid();
+        Ok(id)
+    }
+}
