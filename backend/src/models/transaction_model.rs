d9da445b57c1fae9b24d87ccd7e49cfc1e299e78
Cheng Lin <chlin@pdx.edu>
2023-06-14 15:24:04 -0700

    more comment added

--- backend/src/models/transaction_model.rs
+++ backend/src/models/transaction_model.rs
@@ -2,27 +2,36 @@ use super::super::controllers::transaction_controller::Transaction;
 use super::connect::initialize_database;
 use rusqlite::{params, Result};
 
+// Function to create a new transaction in the database
 pub fn create(amount: f64, description: String, date: String, budget_group_id: i32) -> Result<i32> {
+    // Establishing a connection with the database
     let conn = initialize_database().unwrap();
 
+    // Executing the SQL query to insert the new transaction
     conn.execute(
         "INSERT INTO transactions (amount, description, date, budget_group_id) VALUES (?1, ?2, ?3, ?4)",
         params![amount, description, date, budget_group_id],
     )?;
 
+    // Returning the ID of the last inserted transaction
     Ok(conn.last_insert_rowid() as i32)
 }
 
+// Function to retrieve all transactions associated with a specific budget group
 pub fn get_all(group_id: i32) -> Result<Vec<Transaction>> {
+    // Establishing a connection with the database
     let conn = initialize_database().unwrap();
 
+    // Constructing the SQL query
     let query = format!(
         "SELECT * FROM transactions WHERE budget_group_id = {}",
         group_id
     );
 
+    // Preparing the SQL statement
     let mut stmt = conn.prepare(&query)?;
 
+    // Executing the query and mapping the results to a list of transactions
     let transaction_iter = stmt.query_map([], |row| {
         Ok(Transaction {
             id: row.get(0)?,
@@ -33,20 +42,25 @@ pub fn get_all(group_id: i32) -> Result<Vec<Transaction>> {
         })
     })?;
 
+    // Collecting the transactions into a vector
     let mut transactions = Vec::new();
-
     for transaction in transaction_iter {
         transactions.push(transaction?);
     }
 
+    // Returning the vector of transactions
     Ok(transactions)
 }
 
+// Function to delete a transaction by its ID
 pub fn delete(id: i32) -> Result<()> {
+    // Establishing a connection with the database
     let conn = initialize_database().unwrap();
 
+    // Executing the SQL query to delete the transaction
     conn.execute("DELETE FROM transactions WHERE id = ?1", params![id])?;
 
+    // Returning an Ok result upon successful deletion
     Ok(())
 }
 
