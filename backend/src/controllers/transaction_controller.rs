7ba271d4538799d97bc98adf485feff7f58b32b1
Cheng Lin <chlin@pdx.edu>
2023-06-14 15:43:12 -0700

    comments added

--- backend/src/controllers/transaction_controller.rs
+++ backend/src/controllers/transaction_controller.rs
@@ -4,6 +4,7 @@ use chrono::Utc;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 
+/// Struct representing a Transaction object
 #[derive(Serialize, Deserialize)]
 pub struct Transaction {
     pub id: i32,
@@ -13,6 +14,7 @@ pub struct Transaction {
     pub budget_group_id: i32,
 }
 
+/// Struct to receive data for a new transaction from a form
 #[derive(Deserialize)]
 pub struct TransactionData {
     amount: f64,
@@ -20,6 +22,7 @@ pub struct TransactionData {
     budget_group_id: i32,
 }
 
+/// Returns all transactions for a given budget group ID
 pub async fn get_transactions(path: web::Path<i32>) -> impl Responder {
     let id = path.into_inner();
     match transaction_model::get_all(id) {
@@ -28,19 +31,20 @@ pub async fn get_transactions(path: web::Path<i32>) -> impl Responder {
     }
 }
 
+/// Adds a new transaction for a given budget group ID
 pub async fn add_transaction(req_body: web::Json<TransactionData>) -> impl Responder {
     let date = Utc::now().format("%Y-%m-%d %H:%M").to_string();
     match transaction_model::create(
-        req_body.amount.clone(),
+        req_body.amount,
         req_body.description.clone(),
         date,
-        req_body.budget_group_id.clone(),
+        req_body.budget_group_id,
     ) {
         Ok(id) => {
             // update the remaining budget in the group
             match budget_group_model::minus_remaining(
-                req_body.budget_group_id.clone(),
-                req_body.amount.clone(),
+                req_body.budget_group_id,
+                req_body.amount,
             ) {
                 Ok(()) => HttpResponse::Ok().json(json!({ "id": id })),
                 Err(e) => {
@@ -56,13 +60,14 @@ pub async fn add_transaction(req_body: web::Json<TransactionData>) -> impl Respo
     }
 }
 
+/// Deletes a transaction given its ID and updates the corresponding budget group's remaining budget
 pub async fn delete_transaction(req_body: web::Json<Transaction>) -> impl Responder {
     let id = req_body.id;
     let amount = req_body.amount;
     let budget_group_id = req_body.budget_group_id;
     match transaction_model::delete(id) {
         Ok(_) => {
-            match budget_group_model::plus_remaining(budget_group_id.clone(), amount.clone()) {
+            match budget_group_model::plus_remaining(budget_group_id, amount) {
                 Ok(()) => HttpResponse::Ok().json(json!({"status": "success"})),
                 Err(e) => {
                     println!("Error updating remaining budget: {:?}", e);
